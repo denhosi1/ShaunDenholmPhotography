ef595ea5f7bcaa3f132938d86f2ef12cf83d70ec
Shaun Denholm <ShaunD@intergen.org.nz>
2017-12-18 07:42:05 +1300

    Show all images and create new image through frontend

--- images_api/public/app.js
+++ images_api/public/app.js
@@ -1,3 +1,43 @@
 $(document).ready(function(){
+    $.getJSON("/api/galleryImages")
+    .then(addGalleryImages)
 
+    $('#createButton').click(function(event) {
+        createNewGalleryImage();
+    })
 });
+
+function addGalleryImages(galleryImages) {
+    galleryImages.forEach(function(galleryImage){
+        addGalleryImage(galleryImage);
+    });
+}
+
+function createNewGalleryImage() {
+    var newGalleryImageTitle = $('#galleryImageTitle').val();
+    var newGalleryImageFilePath = $('#galleryImageFilePath').val();
+    var newGalleryImageLocation = $('#galleryImageLocation').val();
+    var newGalleryImageCategories = $('#galleryImageCategories').val();
+
+    $.post('/api/galleryImages', {title: newGalleryImageTitle, filePath: newGalleryImageFilePath, location: newGalleryImageLocation, categories: newGalleryImageCategories})
+    .then(function(newGalleryImage) {
+        if(newGalleryImageTitle != "" && newGalleryImageFilePath != "" && newGalleryImageLocation != "" && newGalleryImageCategories != ""){
+            addGalleryImage(newGalleryImage);
+            $('#galleryImageTitle').val('');
+            $('#galleryImageFilePath').val('');
+            $('#galleryImageLocation').val('');
+            $('#galleryImageCategories').val('');
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function addGalleryImage(galleryImage){
+    var newGalleryImage = $('<li class="task">' +  galleryImage.title + '<span>X</span></li>');
+    if(newGalleryImage.complete) {
+        newGalleryImage.addClass("done");
+    }
+    $('.list').append(newGalleryImage);
+}
